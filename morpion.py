368699fad56cc969841d1d32be244606be434235
Antonin <72139924+4ntonin@users.noreply.github.com>
2021-02-03 14:24:06 +0100

    Correction de bug

--- morpion.py
+++ morpion.py
@@ -1,67 +1,66 @@
 class CMorpion:
     def __init__(self):
-        self.grille = [['-', '-', '-'],
-                       ['-', '-', '-'],
-                       ['-', '-', '-']]
-        self.joueur = 0
-        self.tour = 0
-        self.verif_tour = 0
+        self.__grille = [['-', '-', '-'],
+                         ['-', '-', '-'],
+                         ['-', '-', '-']]
+        self.__joueur = 1
+        self.__tour = 0
+        self.__file = CFile(9)
 
-    def afficher_grille(self):
-        """affiche la grille"""
-        for i in range(3):
-            print(self.grille[i])
+    def get_joueur(self):
+        return self.__joueur
+    
+    def get_tour(self):
+        return self.__tour
 
-    def verification(self, ligne, colonne):
-        """vérifie si les lignes et colonnes entrées par le joueur peuvent être placées dans la grille"""
-        self.verif_tour += 1
-        if 0 > ligne > 3 or 0 > colonne > 3:
-            return print("Erreur, re-essayez.")
-        if self.grille[ligne - 1][colonne - 1] != '-':
-            return print("Erreur, re-essayez.")
-        self.jouer(ligne, colonne)
+    def get_case(self, ligne, colonne):
+        return self.__grille[ligne][colonne]
+
+    def changer_joueur(self):
+        if Morpion.__joueur == 1:
+            Morpion.__joueur = 2
+        else:
+            Morpion.__joueur = 1
 
     def jouer(self, ligne, colonne):
-        """change l'endroit de la grillle voulu par le joueur en 'X' ou 'O' selon qui joue"""
-        if self.joueur == 1:
-            self.grille[ligne - 1][colonne - 1] = 'X'
+        """vérifie si les lignes et colonnes entrées par le joueur peuvent être placées dans la grille"""
+        if ligne > 3 or ligne < 1 or colonne > 3 or colonne < 1:
+            return False
+        if self.__grille[ligne - 1][colonne - 1] != '-':
+            return False
+        if self.__joueur == 1:
+            self.__grille[ligne - 1][colonne - 1] = 'X'
         else:
-            self.grille[ligne - 1][colonne - 1] = 'O'
-        self.afficher_grille()
-        self.tour += 1
-
-    def lancer_partie(self):
-        """lance la partie"""
-        while self.joueur != 1 and self.joueur != 2:
-            self.joueur = int(input("Quel joueur commence (1 ou 2) : "))
-        while self.tour != 9:
-            self.verif_tour = self.tour
-            self.verification(int(input("Ligne ? ")), int(input("Colonne ? ")))
-            if self.condition_fin():
-                print("Le joueur " + str(self.joueur) + " a gagné !")
-                break
-            if self.verif_tour == self.tour:
-                if self.joueur == 1:
-                    self.joueur = 2
-                else:
-                    self.joueur = 1
-        if not self.condition_fin():
-            print("Match nul !")
+            self.__grille[ligne - 1][colonne - 1] = 'O'
+        self.__tour += 1
+        return True
 
     def condition_fin(self):
         """conditions pour que quelqu'un gagne"""
         for i in range(3):
-            if self.grille[i][0] == self.grille[i][1] == self.grille[i][2] != '-':
+            if self.__grille[i][0] == self.__grille[i][1] == self.__grille[i][2] != '-':
                 return True
         for i in range(3):
-            if self.grille[0][i] == self.grille[1][i] == self.grille[2][i] != '-':
+            if self.__grille[0][i] == self.__grille[1][i] == self.__grille[2][i] != '-':
                 return True
-        if self.grille[0][0] == self.grille[1][1] == self.grille[2][2] != '-':
+        if self.__grille[0][0] == self.__grille[1][1] == self.__grille[2][2] != '-':
             return True
-        if self.grille[0][2] == self.grille[1][1] == self.grille[2][0] != '-':
+        if self.__grille[0][2] == self.__grille[1][1] == self.__grille[2][0] != '-':
             return True
         return False
 
 
 Morpion = CMorpion()
-Morpion.lancer_partie()
+
+while Morpion.get_tour() != 9 and not Morpion.condition_fin():
+    while Morpion.jouer(int(input("Ligne ? ")), int(input("Colonne ? "))) == False:
+        print("Erreur, rejouez.")
+    for i in range(3):
+        for j in range(3):
+            print(Morpion.get_case(i, j), ' ' , end='')
+        print('')
+    Morpion.changer_joueur()
+if not Morpion.condition_fin():
+    print("Match nul !")
+else:
+    print("Le joueur " + str(Morpion.get_joueur()) + " a gagné !")
